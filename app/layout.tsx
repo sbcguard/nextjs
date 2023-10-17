c461375def106f565cbd33a9ddcc3e8724a55fbf
AJ Rugen <ARugen@boonecountymo.org>
2023-10-17 13:05:11 -0500

    'autosave'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "./NavBar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
+        <NavBar />
         <main className="p-5">{children}</main>
       </body>
     </html>
