171d406dcbf2a6f9a4c48bc53bb1577898cebcf8
AJ Rugen <ARugen@boonecountymo.org>
2023-10-17 16:55:44 -0500

    'autosave'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import '@radix-ui/themes/styles.css';
+import "@radix-ui/themes/styles.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme } from '@radix-ui/themes';
+import { Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
