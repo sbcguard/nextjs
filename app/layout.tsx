5801c0654d3731c62d042c463a464f29bb20bdd9
AJ Rugen <ARugen@boonecountymo.org>
2023-10-18 07:43:58 -0500

    radix theme customized

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import "@radix-ui/themes/styles.css";
+import './theme-config.css';
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme } from "@radix-ui/themes";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme>
+        <Theme appearance="light" radius="large">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
